d4fcdc710b71c365d2f330f40940d4c16e3bf193
Pasupathi-M <pasupathi.muthukumar@kprinfo.com>
2023-05-20 15:26:57 +0530

    file upload

--- src/controller/google.drive.api.ts
+++ src/controller/google.drive.api.ts
@@ -1,17 +1,36 @@
-import { Response, Request } from 'express'
-
+import { Response, Request } from "express";
+import GoogleDriveApiService from "../servies/google.drive.service";
+import { removeFileWithPath, readtAndWrite } from '../utils/filesHandles'
 class GoogleApis {
-    constructor() {
-        //
+  
+
+  constructor() {}
+
+  async filrUploadtoGDrive(req: Request | any, res: Response) {
+    const GDriveService = new GoogleDriveApiService();
+    try {
+      let filePath = `${__dirname}\\..\\dev-data\\file-upload.json`
+      const response = await GDriveService.uploadFile({ ...req.files });
+      
+      const writeJson = await readtAndWrite({
+        readPath: filePath,
+        writePath: filePath,
+        writeData: response
+      })
+      res.send({
+        message: "Success",
+        cred: process.env.CLIENT_ID,
+        data: response,
+        createOrUpdateSuccess: writeJson
+      })
+    } catch (e: any) {
+      console.log("ERROR", e);
+      res.send({
+        status: false,
+        message: e.message,
+      });
     }
-
-    async testFunction(req: Request, res: Response) {
-        console.log('testFunction')
-        res.send({
-            message: 'Success'
-        })
-    } 
-
+  }
 }
 
-export default new GoogleApis()
+export default new GoogleApis();
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,23 @@
 import  express,{ Router } from 'express'
 import cors from 'cors';
-require('dotenv').config()
+import 'dotenv/config'
 import AppRoutes from './routes/routes.index'
+import fileupload from 'express-fileupload'
+
 const app = express()
 
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
 app.use(cors())
 app.use(express.static('public'))
+
+//File uploaded middleware for express
+app.use(fileupload({
+    uploadTimeout: 0,
+    useTempFiles: true,
+    tempFileDir: `${__dirname}/../temp`
+
+}))
 
 app.use(AppRoutes(express))
 
